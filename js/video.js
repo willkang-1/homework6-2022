a55bda8a871494010b5442c676032552bef3f372
Will <71604938+willkang-1@users.noreply.github.com>
2022-04-14 04:37:20 -0400

    Testing

--- js/video.js
+++ js/video.js
@@ -1,11 +1,18 @@
 var video;
+var play, pause;
 
 window.addEventListener("load", function() {
-	console.log("Good job opening the window")
-
+	console.log("Good job opening the window");
+	video = document.getElementById("player1");
+	video.pause();
+	video.loop = false;
+	console.log("here")
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+});
 
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+});
